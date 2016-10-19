fb550fdb3af1c92f132a821d8ffd3cd598ac9a11
Chris Churas <churas@gmail.com>
2016-10-19 12:06:23 -0500

    completed unit testing of MarkersFromIMODFiducialFileFactory class

--- tests/test_markersfromimodfiducialfilefactory.py
+++ tests/test_markersfromimodfiducialfilefactory.py
@@ -13,15 +13,10 @@ import sys
 import unittest
 import os.path
 import tempfile
-import math
+import stat
 import shutil
 
-from etspecutil.marker import Marker
-from etspecutil.marker import MarkersList
-
 from etspecutil.marker import MarkersFromIMODFiducialFileFactory
-from etspecutil.marker import InvalidAngleError
-from etspecutil.marker import CommonByIndexMarkersListFilter
 
 
 class TestMarkersFromIMODFiducialFileFactory(unittest.TestCase):
@@ -70,6 +65,43 @@ class TestMarkersFromIMODFiducialFileFactory(unittest.TestCase):
                 self.assertTrue(str(e).startswith('Non zero exit code when '
                                                   'running : false -float '
                                                   '-contour ' + fid_file))
+        finally:
+            shutil.rmtree(temp_dir)
+
+    def test_get_markers_model2point_succeeds(self):
+        temp_dir = tempfile.mkdtemp()
+        try:
+            fid_file = os.path.join(temp_dir, 'somefile.txt')
+            open(fid_file, 'a').close()
+            mfac = MarkersFromIMODFiducialFileFactory(fid_file)
+
+            fakemodel2point = os.path.join(temp_dir, 'model2point.py')
+
+            f = open(fakemodel2point, 'w')
+            f.write('#!/usr/bin/env python\n\n')
+            f.write('import sys\n')
+            f.write('f = open(sys.argv[4], "w")\n')
+            f.write('f.write("     1      188.52      283.08        0.00\\n")\n')
+            f.write('f.write("     1      192.29      283.06        1.00\\n")\n')
+            f.write('f.write("     1      196.04      283.04        2.00\\n")\n')
+            f.write('f.close()\n')
+            f.flush()
+            f.close()
+            os.chmod(fakemodel2point, stat.S_IRWXU)
+            print 'XXXXXX',temp_dir
+            # import time
+            # time.sleep(1000)
+            mfac.set_model2point_binary(fakemodel2point)
+
+            mlist = mfac.get_markers()
+            self.assertEqual(len(mlist.get_markers()), 3)
+            m = mlist.get_markers()[0]
+            self.assertEqual(m.get_x(), 188.52)
+            m = mlist.get_markers()[1]
+            self.assertEqual(m.get_x(), 192.29)
+            m = mlist.get_markers()[2]
+            self.assertEqual(m.get_x(), 196.04)
+
         finally:
             shutil.rmtree(temp_dir)
 
